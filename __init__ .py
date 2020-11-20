16b8a3fa8164efb9969bce3fa447f0cf5c4a4301
taylor-lindsay <taylor_lindsay@uri.edu>
2020-11-20 09:38:27 -0500

    Initial Commit

---
+++ __init__ .py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+basic_stats Package for Python
+    Version 1.0 Nov. 18, 2020
+    Author:  Taylor Lindsay, Biological Sciences, URI.
+    Email: taylor_lindsay@uri.edu
+    # 
+    # DISCLAIMER:
+    #    This software is provided "as is" without warranty of any kind. 
+    #=========================================================================      
+"""
+
+#import numpy as np
+from .basic-stats import *
+
+
+__all__ = ['sat_vp']
